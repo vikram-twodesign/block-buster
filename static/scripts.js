a76bd45e57e983031b4a6aec983cbc4bccae47a8
vikram-twodesign <131342697+vikram-twodesign@users.noreply.github.com>
2023-04-26 11:50:55 +0530

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,16 +1,18 @@
 document.getElementById("generate-prompt-btn").addEventListener("click", function () {
   document.getElementById("prompt-output").innerHTML = "Generated Prompt: " + generatePrompt();
-  document.getElementById("user-response-input").disabled = false;
   startTimer(5 * 60); // 5 minutes timer
 });
 
 document.getElementById("submit-response-btn").addEventListener("click", function () {
   document.getElementById("success-message").innerHTML = "Your response has been submitted!";
-  document.getElementById("user-response-input").disabled = true;
+  const inputBox = document.getElementById("user-response-input");
+  inputBox.setAttribute("readonly", "readonly");
 });
 
 function generatePrompt() {
-        "Write a short story about a world where everyone can control one of the four elements: water, earth, air, or fire.",
+  // You can replace this array with your own set of prompts
+  const prompts = [
+    "Write a short story about a world where everyone can control one of the four elements: water, earth, air, or fire.",
         "Compose a poem about the moon and its influence on human emotions.",
         "Create a narrative about a character who discovers they have the power to travel through time.",
         "Write a flash fiction piece about a conversation between the wind and the sea.",
@@ -107,6 +109,11 @@ function generatePrompt() {
         "Write a short story about a character who encounters their doppelganger.",
         "Create a narrative about a character who is transported to a world where their favorite book comes to life.",
         "Write a flash fiction piece about a character who discovers a secret about their own identity.",
+
+  ];
+
+  const randomIndex = Math.floor(Math.random() * prompts.length);
+  return prompts[randomIndex];
 }
 
 function startTimer(duration) {
@@ -124,7 +131,6 @@ function startTimer(duration) {
 
     if (timeLeft < 0) {
       clearInterval(countdown);
-      document.getElementById("user-response-input").disabled = true;
     }
   }, 1000);
 }
